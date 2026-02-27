16df8182c1a37240466c5691efcb65aafcf42456
DilshaNimeshika <dilshanimeshika@gmail.com>
2026-02-27 11:41:12 +0530

    Add Tailwind Basics

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,26 @@
 import { Button } from "@/components/ui/button";
 
 import { ArrowUpIcon } from "lucide-react";
-import Image from "next/image";
-
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
         <h1 className="test-ln font-bold capitalize">
-          Here&apos;s the button shadcn/ui button</h1>
+          Here&apos;s the button shadcn/ui button
+        </h1>
 
-          <div className="flex gap-2">
-            <Button>Click Me!</Button>
-            <Button variant="outline">Click Me!</Button>
-            <Button variant="outline" size="icon" aria-label="Submit">
+        <div className="flex gap-2">
+          <Button>Click Me!</Button>
+          <Button variant="outline">Click Me!</Button>
+          <Button variant="outline" size="icon" aria-label="Submit">
             <ArrowUpIcon />
-            </Button>
-            <Button variant="destructive">Destructive</Button>
-            <Button variant="secondary">Secondary</Button>
-            <Button variant="ghost">Ghost</Button>
-            <Button variant="link">Link</Button>
-
-            
-            
-            
-
-          </div>
-        
-
+          </Button>
+          <Button variant="destructive">Destructive</Button>
+          <Button variant="secondary">Secondary</Button>
+          <Button variant="ghost">Ghost</Button>
+          <Button variant="link">Link</Button>
+        </div>
       </main>
     </div>
   );
---
+++ components/ui/page.tsx
@@ -0,0 +1,9 @@
+export default function home() {
+    return(
+        <div className="flex flex-col min-h-screen">
+        <header className="flex-1 flex items-center justify-center bg-blue-200">
+            <span>I&apos;m the header here</span>
+        </header>
+        </div>
+    );
+}
